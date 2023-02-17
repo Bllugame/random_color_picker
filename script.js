7b1da1c22e5633c5d452a71062c9fe1ea9faf4b8
Matěj Koníček <111693752+matejkonicek@users.noreply.github.com>
2023-02-17 19:44:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function randomcolor() {
-    let color = "#"
+    const color = [];
     for (let i = 0; i < 6; i++) {
-        let rnd = (Math.floor(Math.random() * 16));
-        color += rnd.toString(16);
+        const rnd = Math.floor(Math.random() * 16);
+        color.push(rnd.toString(16));
     }
-    document.body.style.backgroundColor = color;
-    document.getElementById("paragraph").innerHTML = color.toUpperCase();
+    const hexColor = "#" + color.join("");
+    document.body.style.backgroundColor = hexColor;
+    document.getElementById("paragraph").innerHTML = hexColor.toUpperCase();
 }
